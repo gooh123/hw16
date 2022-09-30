3fefb37299fcee0c2439a02af0573fa1a994e6a2
gooh123 <101920016+gooh123@users.noreply.github.com>
2022-09-30 14:05:08 +1000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,14 +7,11 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
 
-    db.init_app(app)
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
     with app.app_context():
-
+        db.init_app(app)
         from app import routes
-
         db.create_all()
-
         return app
+
